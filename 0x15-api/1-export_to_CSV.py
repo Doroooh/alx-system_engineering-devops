f3b12a2ec67d45e894f124b3b1c1798a596ec725
Waithira <50845913+Doroooh@users.noreply.github.com>
2024-07-29 22:56:55 +0300

    api project

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -9,26 +9,26 @@ import csv
 
 if __name__ == "__main__":
     response = get('https://jsonplaceholder.typicode.com/todos/')
-    employeedata = response.json()
+    data = response.json()
 
-    therow = []
-    secondresponse = get('https://jsonplaceholder.typicode.com/users')
-    emplsecdata = secondresponse.json()
+    row = []
+    response2 = get('https://jsonplaceholder.typicode.com/users')
+    data2 = response2.json()
 
-    for t in emplsecdata:
-        if t['id'] == int(argv[1]):
-            employee = t['username']
+    for i in data2:
+        if i['id'] == int(argv[1]):
+            employee = i['username']
 
     with open(argv[1] + '.csv', 'w', newline='') as file:
         writ = csv.writer(file, quoting=csv.QUOTE_ALL)
 
-        for t in employeedata:
+        for i in data:
 
-            therow = []
-            if t['userId'] == int(argv[1]):
-                therow.append(t['userId'])
-                therow.append(employee)
-                therow.append(t['completed'])
-                therow.append(t['title'])
+            row = []
+            if i['userId'] == int(argv[1]):
+                row.append(i['userId'])
+                row.append(employee)
+                row.append(i['completed'])
+                row.append(i['title'])
 
                 writ.writerow(row)
