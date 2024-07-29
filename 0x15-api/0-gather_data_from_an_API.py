e72d4c795c0372f1743f1b5fed6100fd40dfa34e
Waithira <50845913+Doroooh@users.noreply.github.com>
2024-07-29 22:59:28 +0300

    api project

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 """
-Python script that utilizes an API to retrieve an employees to do with the employee ID
+Python script that, using a REST API, for a given employee ID,
+returns information about his/her TODO list progress.
 """
 
 from requests import get
@@ -13,24 +14,24 @@ if __name__ == "__main__":
     data = response.json()
     completed = 0
     total = 0
-    tasks = []
-    response2 = get('https://jsonplaceholder.typicode.com/users')
-    data2 = response2.json()
+    todotasks = []
+    responsetwo = get('https://jsonplaceholder.typicode.com/users')
+    datatwo = responsetwo.json()
 
-    for i in data2:
-        if i.get('id') == int(argv[1]):
-            employee = i.get('name')
+    for k in datatwo:
+        if k.get('id') == int(argv[1]):
+            employee = k.get('name')
 
-    for i in data:
-        if i.get('userId') == int(argv[1]):
+    for k in data:
+        if k.get('userId') == int(argv[1]):
             total += 1
 
-            if i.get('completed') is True:
+            if k.get('completed') is True:
                 completed += 1
-                tasks.append(i.get('title'))
+                todotasks.append(k.get('title'))
 
-    print("Employee {} is done with tasks({}/{}):".format(employee, completed,
+    print("Employee {} is done with todotasks({}/{}):".format(employee, completed,
                                                           total))
 
-    for i in tasks:
-        print("\t {}".format(i))
+    for k in todotasks:
+        print("\t {}".format(k))
