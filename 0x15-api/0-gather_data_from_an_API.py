d27fd49c21e0bf326deda10a9190ab6d2e176e34
Waithira <50845913+Doroooh@users.noreply.github.com>
2024-07-29 22:55:33 +0300

    api project

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -12,25 +12,25 @@ if __name__ == "__main__":
     response = get('https://jsonplaceholder.typicode.com/todos/')
     data = response.json()
     completed = 0
-    ttl = 0
+    total = 0
     tasks = []
-    responsetwo = get('https://jsonplaceholder.typicode.com/users')
-    datatwo = responsetwo.json()
+    response2 = get('https://jsonplaceholder.typicode.com/users')
+    data2 = response2.json()
 
-    for k in datatwo:
-        if k.get('id') == int(argv[1]):
-            employee = k.get('name')
+    for i in data2:
+        if i.get('id') == int(argv[1]):
+            employee = i.get('name')
 
-    for k in data:
-        if k.get('userId') == int(argv[1]):
+    for i in data:
+        if i.get('userId') == int(argv[1]):
             total += 1
 
-            if k.get('completed') is True:
+            if i.get('completed') is True:
                 completed += 1
-                tasks.append(k.get('title'))
+                tasks.append(i.get('title'))
 
     print("Employee {} is done with tasks({}/{}):".format(employee, completed,
-                                                          ttl))
+                                                          total))
 
-    for k in tasks:
-        print("\t {}".format(k))
+    for i in tasks:
+        print("\t {}".format(i))
