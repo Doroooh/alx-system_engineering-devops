56645a5170b9c0c26cbf7dd344a833972f5964b1
Waithira <50845913+Doroooh@users.noreply.github.com>
2024-08-13 23:04:48 +0300

    API ADVANCED

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,31 +1,28 @@
 #!/usr/bin/python3
-"""Module that consumes the Reddit API and returns a list containing the
-titles of all hot articles for a given subreddit."""
+""" This is a module to consume Reddit API, will return list that contains titles of all hot articles for given subreddit."""
 import requests
 
-
 def recurse(subreddit, hot_list=[], n=0, after=None):
-    """ queries the Reddit API and returns a list containing the titles of
-    all hot articles for a given subreddit
-
-    The Reddit API uses pagination for separating pages of responses.
-    If not a valid subreddit, return None.
+    """ This will query Reddit API, to return list with titles for
+    all hot articles for given subreddit. 
+    Reddit API utilizes pagination to separate the page responses.
+    If the subreddit isn't valid, return None. 
 
     Args:
         subreddit (str): subreddit.
-        hot_list (list, optional): list of titles. Defaults to [].
+        hot_list (list, optional): the list of the article titles. Defaults to [].
 
     Returns:
-        list: list of titles.
+        list: to return list of titles.
     """
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
     headers = {'user-agent': 'custom'}
-    r = requests.get(url, headers=headers, allow_redirects=False)
-    if r.status_code == 200:
-        r = r.json()
-        for post in r.get('data').get('children'):
+    hawih = requests.get(url, headers=headers, allow_redirects=False)
+    if hawih.status_code == 200:
+        hawih = hawih.json()
+        for post in hawih.get('data').get('children'):
             hot_list.append(post.get('data').get('title'))
-        if r.get('data').get('after'):
+        if hawih.get('data').get('after'):
             recurse(subreddit, hot_list)
         return hot_list
     else:
