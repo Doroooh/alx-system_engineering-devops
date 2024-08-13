c606b8c5c307bf8263548b43481fd7cfda490fc4
Waithira <50845913+Doroooh@users.noreply.github.com>
2024-08-13 23:32:36 +0300

    API ADVANCED

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,39 +1,29 @@
-!/usr/bin/python3
-"""Defining a recurse function"""
+#!/usr/bin/python3
+"""Module that consumes the Reddit API and returns a list containing the
+titles of all hot articles for a given subreddit."""
 import requests
 
 
-def get_title(children):
-    """Returning children's title"""
-    return children.get("data").get("title")
-
-
-def recurse(subreddit, hot_list=[], after=None):
+def recurse(subreddit, hot_list=[], n=0, after=None):
+    """ query Reddit API and returns a list containing the titles of
+    all hot articles for a given subreddit
+    The Reddit API uses pagination for separating pages of responses.
+    If not a valid subreddit, return None.
+    Args:
+        subreddit (str): subreddit.
+        hot_list (list, optional): list of titles. Defaults to [].
+    Returns:
+        list: list of titles.
     """
-    Query Reddit API, return list that contains titles of
-    all hot articles for a given subreddit.
-    - If no results are found for given subreddit,
-    the function should return None
-    """
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x016.project:v1.0.0 (by /u/ecalvoc)"
-        }
-    params = {"limit": 100}
-    if after:
-        params["after"] = after
-
-    hot_data = requests.get(url,
-                            headers=headers,
-                            params=params,
-                            allow_redirects=False).json().get("data")
-    if not hot_data:
-        return
-
-    child = hot_data.get("children")
-    hot_list.extend(list(map(get_title, child)))
-
-    after = hot_data.get("after")
-    if not after:
+    url = 'https://www.reddit.com/hawih/{}/hot.json'.format(subreddit)
+    headers = {'user-agent': 'custom'}
+    hawih = requests.get(url, headers=headers, allow_redirects=False)
+    if hawih.status_code == 200:
+        hawih = hawih.json()
+        for post in hawih.get('data').get('children'):
+            hot_list.append(post.get('data').get('title'))
+        if hawih.get('data').get('after'):
+            recurse(subreddit, hot_list)
         return hot_list
-    return recurse(subreddit, hot_list, after)
+    else:
+        return None
