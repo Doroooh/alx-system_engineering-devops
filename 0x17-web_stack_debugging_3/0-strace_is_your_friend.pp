15acc1821026bbd2164b525e77ba8fef8134edf3
Waithira <50845913+Doroooh@users.noreply.github.com>
2024-08-16 00:32:09 +0300

    debug3

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
-# Creating manifest to fix file name typo
-exec { 'fix_typo':
-  command => '/bin/mv /var/www/html/wp-includes/class-wp-locale.php /var/www/html/wp-includes/class-wp-locale.phpp',
-  onlyif  => '/bin/test -f /var/www/html/wp-includes/class-wp-locale.php',
+# Manifest to fix apache configuration
+exec { 'fix config file':
+  command =>  "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
+  path    => '/bin'
 }
